712b1a71a823f590ebb87c03d3050b8f002619a4
DevMasterX <sokol080288@gmail.com>
2024-12-05 17:12:44 +0200

    task 2 in progress

---
+++ src/js/02-timer.js
@@ -0,0 +1,55 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const startBtn = document.querySelector('[data-start]');
+const days = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
+let targetDate = 0;
+console.log('🚀  targetDate:', targetDate);
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    targetDate = selectedDates[0].getTime();
+    // console.log(selectedDates[0].getTime());
+    if (targetDate <= Date.now()) {
+      window.alert('Please choose a date in the future');
+    }
+
+    if (selectedDates[0].getTime() > Date.now()) {
+      startBtn.disabled = false;
+    }
+  },
+};
+
+startBtn.disabled = true;
+
+startBtn.addEventListener('click', onStartBtnClick);
+
+flatpickr('#datetime-picker', options);
+
+function onStartBtnClick() {}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
