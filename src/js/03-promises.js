dd8f3e1d9f3422c30fdb6bbd0b59a7c56b525677
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2024-12-09 16:01:33 +0200

    43214

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,3 +6,65 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+// ------------------------------------
+
+// const promise = new Promise((res, rej) => {
+//   setTimeout(() => {
+//     const val = Math.random();
+
+//     if (val > 0.5) {
+//       res('yesssss');
+//     } else {
+//       rej('Nooooooooo');
+//     }
+//   }, 3000);
+// });
+// console.log(promise);
+
+// promise
+//   .then(val => {
+//     return val + '😁';
+//   })
+//   .then(val => console.log(val))
+//   .catch(err => console.error(err))
+//   .finally(() => console.log('Finally'));
+
+// const promise = fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+// console.log('🚀  promise:', promise);
+
+// promise
+//   .then(resp => resp.json())
+//   .then(data => console.log(data))
+//   .catch(err => console.error(err));
+
+const start = document.querySelector('.js-start');
+const container = document.querySelector('.js-container');
+
+start.addEventListener('click', onStart);
+
+function onStart() {
+  [...container.children].forEach((box, i) => (box.textContent = ''));
+  [...container.children].forEach((box, i) => {
+    return createPromise(i)
+      .then(smile => {
+        box.textContent = smile;
+      })
+      .catch(smile => {
+        box.textContent = smile;
+      });
+  });
+}
+
+function createPromise(delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const random = Math.random();
+      if (random > 0.55) {
+        resolve('💵');
+      } else {
+        reject('❌');
+      }
+    }, 1000 * delay + 1000);
+  });
+}
