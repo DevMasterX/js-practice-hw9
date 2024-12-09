32c95cbde605ad76b54b1f41b3c30ee7a6d1ce09
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2024-12-09 14:15:41 +0000

    Deploying to gh-pages from @ DevMasterX/js-practice-hw9@fe8a7a168f41f767a2bf389fdeeafe6c4f95d779 🚀

--- 03-promises.1408a847.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector(".js-start"),e=document.querySelector(".js-container");t.addEventListener("click",(function(){[...e.children].forEach(((t,e)=>t.textContent="")),[...e.children].forEach(((t,e)=>{return(n=e,new Promise(((t,e)=>{setTimeout((()=>{Math.random()>.55?t("💵"):e("❌")}),1e3*n+1e3)}))).then((e=>{t.textContent=e})).catch((e=>{t.textContent=e}));var n}))}));
-//# sourceMappingURL=03-promises.1408a847.js.map
