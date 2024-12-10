4657c4186bbd406fdb97180cfe00a04743979f37
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2024-12-10 11:28:13 +0200

    task 3 - promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,11 +1,45 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const firstDelay = Number(e.target.delay.value);
+  const delayStep = Number(e.target.step.value);
+  const amount = Number(e.target.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    const delay = firstDelay + delayStep * (i - 1);
+
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+// createPromise(2, 500)
+//   .then(item => console.log(item))
+//   .catch(error => console.error(error));
 
 // ------------------------------------
 
@@ -55,46 +89,46 @@ function createPromise(position, delay) {
 
 // ----------------------------------------------------------------
 
-const start = document.querySelector('.js-start');
-const container = document.querySelector('.js-container');
+// const start = document.querySelector('.js-start');
+// const container = document.querySelector('.js-container');
 
-start.addEventListener('click', onStart);
+// start.addEventListener('click', onStart);
 
-function onStart() {
-  let counter = 0;
+// function onStart() {
+//   let counter = 0;
 
-  [...container.children].forEach((box, i) => (box.textContent = ''));
-  const promises = [...container.children].map((_, i) => createPromise(i));
+//   [...container.children].forEach((box, i) => (box.textContent = ''));
+//   const promises = [...container.children].map((_, i) => createPromise(i));
 
-  Promise.allSettled(promises).then(items => {
-    items.forEach((item, i) => {
-      setTimeout(() => {
-        if (item.status === 'fulfilled') {
-          counter += 1;
-        }
-        container.children[i].textContent = item.value || item.reason;
+//   Promise.allSettled(promises).then(items => {
+//     items.forEach((item, i) => {
+//       setTimeout(() => {
+//         if (item.status === 'fulfilled') {
+//           counter += 1;
+//         }
+//         container.children[i].textContent = item.value || item.reason;
 
-        if (i === container.children.length - 1) {
-          setTimeout(() => {
-            if (counter === container.children.length || !counter) {
-              alert('Winner 🍒🍒🍒');
-            } else {
-              alert('❌Lost money!!!!😪😪😩');
-            }
-          }, 500);
-        }
-      }, i * 1000);
-    });
-  });
-}
+//         if (i === container.children.length - 1) {
+//           setTimeout(() => {
+//             if (counter === container.children.length || !counter) {
+//               alert('Winner 🍒🍒🍒');
+//             } else {
+//               alert('❌Lost money!!!!😪😪😩');
+//             }
+//           }, 500);
+//         }
+//       }, i * 1000);
+//     });
+//   });
+// }
 
-function createPromise() {
-  return new Promise((resolve, reject) => {
-    const random = Math.random();
-    if (random > 0.55) {
-      resolve('💵');
-    } else {
-      reject('❌');
-    }
-  });
-}
+// function createPromise() {
+//   return new Promise((resolve, reject) => {
+//     const random = Math.random();
+//     if (random > 0.55) {
+//       resolve('💵');
+//     } else {
+//       reject('❌');
+//     }
+//   });
+// }
